48dd2b68aee74d9295d92cdb08acbd361f2ea5d0
K <x00.x7f@gmail.com>
2017-02-06 20:29:34 +0900

    Improved something

--- cmd/golet/main.go
+++ cmd/golet/main.go
@@ -1,35 +1,46 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io"
 	"time"
 
 	"github.com/Code-Hex/golet"
 )
 
 func main() {
-	p := golet.New()
+	/*
+		if err := agent.Listen(nil); err != nil {
+			log.Fatal(err)
+		}
+	*/
+	p := golet.New(context.Background())
 	p.EnableColor()
 	p.SetInterval(time.Second * 0)
 
 	p.Add(
 		golet.Service{
-			Exec:   "ping google.com",
+			Exec: "ping google.com",
+			//Every: "33 * * * * *",
+		},
+		golet.Service{
+			Exec:   "ping ie.u-ryukyu.ac.jp",
 			Worker: 2,
-		},
-		golet.Service{
-			Exec: "ping ie.u-ryukyu.ac.jp",
-			Tag:  "ryukyu",
-		},
-		golet.Service{
-			Exec:   "ping yahoo.co.jp",
-			Tag:    "yahoo",
-			Worker: 3,
 		},
 	)
 
 	p.Add(golet.Service{
+		Code: func(w io.Writer) error {
+			fmt.Fprintln(w, "Hello golet!!")
+			return nil
+		},
+		Worker: 4,
+		Every:  "33 * * * * *",
+	})
+
+	p.Add(golet.Service{
 		Exec:   "ping gigazine.net",
-		Tag:    "gigazine",
 		Worker: 1,
 	})
 
--- golet.go
+++ golet.go
@@ -2,17 +2,22 @@ package golet
 
 import (
 	"bufio"
+	"context"
+	"errors"
 	"fmt"
 	"io"
 	"os"
 	"os/exec"
+	"os/signal"
 	"runtime"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/Code-Hex/golet/internal/port"
 	colorable "github.com/mattn/go-colorable"
 	"github.com/robfig/cron"
+	"golang.org/x/sync/errgroup"
 )
 
 type color int
@@ -24,38 +29,40 @@ const (
 	blue
 	magenta
 	cyan
+
+	colornum int = 5
 )
 
-const colornum = 5
-
 type (
-	// config is main struct
+	// config is main struct.
 	// struct comments from http://search.cpan.org/dist/Proclet/lib/Proclet.pm
-	// Proclet is great library!!
+	// Proclet is great module!!
 	config struct {
-		interval   time.Duration // interval in seconds between spawning services unless a service exits abnormally
-		color      bool          // colored log
+		interval   time.Duration // interval in seconds between spawning services unless a service exits abnormally.
+		color      bool          // colored log.
 		logger     io.Writer     // sets the output destination file. use stderr by default.
 		logWorker  bool          // enable worker for format logs. If disabled this option, cannot use logger opt too.
-		execNotice bool          // enable start and exec notice message like: `16:38:12 worker.1 | Start callback: worker``
+		execNotice bool          // enable start and exec notice message like: `16:38:12 worker.1 | Start callback: worker``.
 
 		services   []Service
+		g          *errgroup.Group
+		ctx        context.Context
 		serviceNum int
 		tags       map[string]bool
 		basePort   int
 		cron       *cron.Cron
 	}
 
-	// Service struct to add services to golet
+	// Service struct to add services to golet.
 	Service struct {
 		Exec   string
-		Code   func() // Routine of services
-		Worker int    // Number of goroutine. The maximum number of workers is 100.
-		Tag    string // Keyword for log.
-		Every  string // Crontab like format.
+		Code   func(io.Writer) error // Routine of services.
+		Worker int                   // Number of goroutine. The maximum number of workers is 100.
+		Tag    string                // Keyword for log.
+		Every  string                // Crontab like format.
 
-		color     int
 		startPort int
+		color     color
 		pipe      pipe
 	}
 
@@ -65,14 +72,15 @@ type (
 	}
 )
 
+// Runner interface have methods for configuration and to run services.
 type Runner interface {
 	SetInterval(time.Duration)
 	EnableColor()
 	SetLogger(io.Writer)
 	DisableLogger()
 	DisableExecNotice()
-	Add(...Service)
-	Run()
+	Add(...Service) error
+	Run() error
 }
 
 var (
@@ -103,8 +111,9 @@ func (c *config) SetLogger(f io.Writer)       { c.logger = f }
 func (c *config) DisableLogger()              { c.logWorker = false }
 func (c *config) DisableExecNotice()          { c.execNotice = false }
 
-// New to create struct of golet
-func New() Runner {
+// New to create struct of golet.
+func New(c context.Context) Runner {
+	g, ctx := errgroup.WithContext(c)
 	return &config{
 		interval:   0,
 		color:      false,
@@ -112,140 +121,186 @@ func New() Runner {
 		logWorker:  true,
 		execNotice: true,
 
+		g:    g,
+		ctx:  ctx,
 		tags: map[string]bool{},
 		cron: cron.New(),
 	}
 }
 
-func (p *config) Add(services ...Service) {
+func (c *config) Env(envs map[string]string) error {
+	for k := range envs {
+		if e := os.Setenv(k, envs[k]); e != nil {
+			return e
+		}
+	}
+	return nil
+}
+
+func (c *config) Add(services ...Service) error {
 	mu.Lock()
 	defer mu.Unlock()
 
 	for _, service := range services {
-		p.serviceNum++
+		c.serviceNum++
 
 		if service.Tag == "" {
-			service.Tag = fmt.Sprintf("%d", p.serviceNum)
+			service.Tag = fmt.Sprintf("%d", c.serviceNum)
 		}
 
 		if service.Worker <= 0 {
 			service.Worker = 1
 		}
 
-		if _, ok := p.tags[service.Tag]; ok {
-			die("tag: %s is already exists", service.Tag)
+		if _, ok := c.tags[service.Tag]; ok {
+			return errors.New("tag: " + service.Tag + " is already exists")
 		}
-		p.tags[service.Tag] = true
+		c.tags[service.Tag] = true
 
 		n, err := port.GetPort()
 		if err != nil {
-			panic(err)
+			return err
 		}
 
 		service.startPort = n
-		service.color = p.serviceNum % colornum
+		service.color = color(c.serviceNum%colornum + 32)
 
-		p.services = append(p.services, service)
+		c.services = append(c.services, service)
 	}
+	return nil
 }
 
-func (p *config) Run() {
+func (c *config) Run() error {
 	services := make(map[string]Service)
 
 	// calculate of the number of workers
 	cap := 0
-	for _, service := range p.services {
+	for _, service := range c.services {
 		cap += service.Worker
 	}
 
 	order := make([]string, 0, cap)
 
 	// assignment services
-	for _, service := range p.services {
+	for _, service := range c.services {
 		worker := service.Worker
 		for i := 1; i <= worker; i++ {
 			in, out, err := os.Pipe()
 			if err != nil {
-				panic(err)
+				return err
 			}
+
 			s := service
 			sid := fmt.Sprintf("%s.%d", s.Tag, i)
 
-			s.pipe.reader = in
-			s.pipe.writer = out
+			s.pipe = pipe{in, out}
 			s.startPort += i
 			services[sid] = s
 			order = append(order, sid)
 		}
 	}
 
-	var wg sync.WaitGroup
+	chps := make(chan *os.Process)
 
-	// enable log worker if logWorker is true
-	if p.logWorker {
-		wg.Add(len(order) + 1)
-		go func() {
-			defer wg.Done()
-			p.logging(services)
-		}()
-	} else {
-		wg.Add(len(order))
-	}
+	signals := make(chan os.Signal)
+	signal.Notify(signals, syscall.SIGHUP, syscall.SIGTERM, syscall.SIGINT)
+
+	go c.waitSignals(signals, chps, len(order))
 
 	// invoke workers
 	for _, sid := range order {
 		service := services[sid]
-		if service.Code == nil && len(service.Exec) > 0 {
-			go func(s Service) {
-				defer wg.Done()
-				s.exec()
-			}(service)
+		if service.Code == nil && service.Exec != "" {
+			cmd := service.prepare()
+			if service.Every != "" {
+				c.cron.AddFunc(service.Every, func() {
+					run(cmd, chps)
+				})
+			} else {
+				c.g.Go(func() error {
+					defer service.pipe.writer.Close()
+					return run(cmd, chps)
+				})
+			}
 		}
-		time.Sleep(p.interval)
-	}
-	wg.Wait()
-}
 
-func (s *Service) exec() {
-	defer s.pipe.writer.Close()
-	args := append(shell, s.Exec)
-	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Env = os.Environ()
-	cmd.Stdout = s.pipe.writer
-	cmd.Stderr = s.pipe.writer
-	cmd.Run()
-}
+		if service.Code != nil {
+			if service.Every != "" {
+				c.cron.AddFunc(service.Every, func() {
+					service.Code(service.pipe.writer)
+				})
+			} else {
+				c.g.Go(func() error {
+					defer service.pipe.writer.Close()
+					return service.Code(service.pipe.writer)
+				})
+			}
+		}
 
-func (p *config) logging(services map[string]Service) {
-	readers := []*os.File{}
-	fileno2sid := map[int]string{}
-
-	for sid, s := range services {
-		reader := s.pipe.reader
-		fileno2sid[int(reader.Fd())] = sid
-		readers = append(readers, reader)
+		// enable log worker if logWorker is true
+		if c.logWorker && (service.Code != nil || service.Exec != "") {
+			rd := service.pipe.reader
+			go c.logging(bufio.NewScanner(rd), sid, service.color)
+		}
+		time.Sleep(c.interval)
 	}
 
-	for _, reader := range readers {
-		go func(sc *bufio.Scanner, sid string) {
-			for sc.Scan() {
-				hour, min, sec := time.Now().Clock()
-				if p.color {
-					fmt.Fprintf(p.logger, "\x1b[%dm%02d:%02d:%02d %-10s |\x1b[0m %s\n",
-						services[sid].color+32,
-						hour, min, sec, sid,
-						sc.Text(),
-					)
-				} else {
-					fmt.Fprintf(p.logger, "%02d:%02d:%02d %-10s | %s\n", hour, min, sec, sid, sc.Text())
+	c.cron.Start()
+
+	return c.g.Wait()
+}
+
+func (c *config) waitSignals(signals <-chan os.Signal, chps <-chan *os.Process, cap int) {
+	procs := make([]*os.Process, 0, cap)
+	for {
+		select {
+		case proc := <-chps:
+			procs = append(procs, proc)
+		case s := <-signals:
+			switch s {
+			case syscall.SIGTERM, syscall.SIGHUP:
+				for _, p := range procs {
+					p.Signal(syscall.SIGTERM)
+				}
+			case syscall.SIGINT:
+				for _, p := range procs {
+					p.Signal(syscall.SIGINT)
 				}
 			}
-		}(bufio.NewScanner(reader), fileno2sid[int(reader.Fd())])
+		case <-c.ctx.Done():
+			c.cron.Stop()
+			return
+		}
 	}
-
 }
 
-func die(msg string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, msg, args...)
-	os.Exit(1)
+func run(c *exec.Cmd, chps chan<- *os.Process) error {
+	if err := c.Start(); err != nil {
+		return err
+	}
+	chps <- c.Process
+	return c.Wait()
+}
+
+func (s *Service) prepare() *exec.Cmd {
+	args := append(shell, s.Exec)
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Stdout = s.pipe.writer
+	cmd.Stderr = s.pipe.writer
+	return cmd
+}
+
+func (c *config) logging(sc *bufio.Scanner, sid string, clr color) {
+	for sc.Scan() {
+		hour, min, sec := time.Now().Clock()
+		if c.color {
+			fmt.Fprintf(c.logger, "\x1b[%dm%02d:%02d:%02d %-10s |\x1b[0m %s\n",
+				clr,
+				hour, min, sec, sid,
+				sc.Text(),
+			)
+		} else {
+			fmt.Fprintf(c.logger, "%02d:%02d:%02d %-10s | %s\n", hour, min, sec, sid, sc.Text())
+		}
+	}
 }
